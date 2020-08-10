3c638f146787f602f5f349529d718af619f7f0c0
sadat <sadat.himel@gmail.com>
2020-08-10 22:26:26 +0600

    null vs undefind clear

---
+++ null-undefined.js
@@ -0,0 +1,17 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return;
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const premik = {name:"smart dude", phone:4568621};
+console.log(premik.name)
+
+let fun = undefined;
+console.log(fun)
+
+let ages = [2, 5,9]
