9e3d2538d2c31a547f3c81023b3655be6975a083
sadat <sadat.himel@gmail.com>
2020-08-10 21:25:39 +0600

    truthy and falsy value

---
+++ true-false.js
@@ -0,0 +1,17 @@
+// Falsy
+// const name = " ";
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truthy
+// '0', " "
+let name = null;
+console.log(name)
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
