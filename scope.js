28319ff3bad9b7c9e5f3251c2b70815913574f89
sadat <sadat.himel@gmail.com>
2020-08-11 13:32:28 +0600

    scope, block scope, access outer scope variable

---
+++ scope.js
@@ -0,0 +1,19 @@
+let bonus = 100;
+
+function sum(first, second){
+      let result = first + second + bonus;
+      console.log(result);
+      if(result > 9){
+            let mood = "happy";
+            mood = "first";
+            mood = "fishy";
+            mood = "funky";
+            mood = "cranky";
+            console.log(mood);
+      }
+      // console.log(mood)
+      return result;
+}
+const output = sum(3, 7);
+// console.log(output);
+// console.log(bonus)
